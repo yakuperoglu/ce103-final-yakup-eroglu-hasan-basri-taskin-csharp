bd14974f591653c69fb717de4ebdcbe4a998b2ef
yakup <yakup_eroglu22@erdogan.edu.tr>
2024-01-11 13:09:40 +0300

    addbook delete book added and doxygen comments writed

--- LibrarysystemLibrary/Librarysystem.cs
+++ LibrarysystemLibrary/Librarysystem.cs
@@ -104,5 +104,109 @@ namespace LibrarysystemLibrary
             }
             return true;
         }
+        /**
+ * @brief Displays the menu for adding a new book and handles the user input.
+ * @param pathFileBooks The file path for storing book information.
+ * @return True to indicate successful execution.
+ */
+        public bool AddBookMenu(string pathFileBooks)
+        {
+            ClearScreen();
+            Console.Write("Enter a book name: ");
+            string bookName = Console.ReadLine();
+            AddBook(bookName, pathFileBooks);
+            return true;
+        }
+        /**
+ * @brief Adds a new book to the library.
+ * @param bookName The name of the book to be added.
+ * @param pathFileBooks The file path for storing book information.
+ * @return True to indicate successful execution.
+ */
+        public bool AddBook(string bookName, string pathFileBooks)
+        {
+            Book newBook = new Book
+            {
+                Id = GetNewId(pathFileBooks),
+                Name = bookName,
+                IsMarked = false,
+                IsWishlist = false,
+                IsLoaned = false
+            };
+            // Write book as binary 
+            using (BinaryWriter writer = new BinaryWriter(File.Open(pathFileBooks, FileMode.Append)))
+            {
+                writer.Write(newBook.Id);
+                writer.Write(newBook.Name);
+                writer.Write(newBook.IsMarked);
+                writer.Write(newBook.IsWishlist);
+                writer.Write(newBook.IsLoaned);
+            }
+
+            return true;
+        }
+        /**
+ * @brief Displays the menu for deleting a book and handles the user input.
+ * @param pathFileBooks The file path for storing book information.
+ * @return True to indicate successful execution.
+ */
+        public bool DeleteBookMenu(string pathFileBooks)
+        {
+            ClearScreen();
+            WriteBooksToConsole(pathFileBooks);
+            Console.Write("Enter a number to delete book: ");
+            int bookId;
+
+            // Check if the entered input is a valid integer.
+            if (!int.TryParse(Console.ReadLine(), out bookId))
+            {
+                HandleInputError();
+                EnterToContinue();
+                return false;
+            }
+
+            // Delete the book with the specified ID.
+            DeleteBook(bookId, pathFileBooks);
+            return true;
+        }
+        /**
+ * @brief Deletes a book from the library based on the provided book ID.
+ * @param bookId The unique identifier of the book to be deleted.
+ * @param pathFileBooks The file path for storing book information.
+ * @return True to indicate successful execution.
+ */
+        public bool DeleteBook(int bookId, string pathFileBooks)
+        {
+            List<Book> books = LoadBooks(pathFileBooks);
+            bool isFound = false;
+            using (BinaryWriter writer = new BinaryWriter(File.Open(pathFileBooks, FileMode.Create)))
+            {
+                foreach (Book book in books)
+                {
+                    if (book.Id != bookId)
+                    {
+                        writer.Write(book.Id);
+                        writer.Write(book.Name);
+                        writer.Write(book.IsMarked);
+                        writer.Write(book.IsWishlist);
+                        writer.Write(book.IsLoaned);
+                    }
+                    else
+                    {
+                        isFound = true;
+                    }
+                }
+            }
+
+            if (isFound)
+            {
+                Console.WriteLine($"Book with ID '{bookId}' has been deleted successfully.");
+                EnterToContinue();
+                return true;
+            }
+            Console.WriteLine($"There is no book you want!");
+            EnterToContinue();
+            return false;
+        }
     }
 }
