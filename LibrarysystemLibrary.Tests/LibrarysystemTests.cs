b00d48c0bd2e092ef69f95fb5274ba14375a1a6c
yakup <yakup_eroglu22@erdogan.edu.tr>
2024-01-11 13:26:06 +0300

    some tests added

--- LibrarysystemLibrary.Tests/LibrarysystemTests.cs
+++ LibrarysystemLibrary.Tests/LibrarysystemTests.cs
@@ -73,6 +73,72 @@ namespace LibrarysystemLibrary.Tests
             Console.SetOut(new StreamWriter(Console.OpenStandardOutput()));
             Console.SetIn(new StreamReader(Console.OpenStandardInput()));
         }
+
+        [Fact]
+        public void LoadBooks_ShouldLoadBooksFromFile()
+        {
+
+            CleanupTestDataBook();
+            CleanupTestDataUser();
+
+            Librarysystem library = new Librarysystem();
+            library.IsTestMode = true;
+            CreateTestFile();
+
+            List<Book> books = library.LoadBooks(testFilePathBooks);
+
+            Assert.NotNull(books);
+            Assert.Equal(4, books.Count());
+
+            // Clean up
+            Console.SetOut(new StreamWriter(Console.OpenStandardOutput()));
+            Console.SetIn(new StreamReader(Console.OpenStandardInput()));
+        }
+
+        [Fact]
+        public void HandleInputError_ShouldReturnFalse()
+        {
+            CleanupTestDataBook();
+            CleanupTestDataUser();
+
+
+            Librarysystem library = new Librarysystem();
+            library.IsTestMode = true;
+
+            bool result = library.HandleInputError();
+
+            Assert.False(result);
+
+            // Clean up
+            Console.SetOut(new StreamWriter(Console.OpenStandardOutput()));
+            Console.SetIn(new StreamReader(Console.OpenStandardInput()));
+        }
+        [Fact]
+        public void TestMain_Menu_ShouldVisitAllFunctionsAndExit()
+        {
+            CleanupTestDataBook();
+            CleanupTestDataUser();
+
+            var input = new StringReader("2\nexample\nexample\n3\n2\n1\nexample\nexample\n5\n4");
+            Console.SetIn(input);
+
+            var output = new StringWriter();
+            Console.SetOut(output);
+
+            var library = new Librarysystem();
+            library.IsTestMode = true;
+
+            int result = library.Main_Menu(testFilePathUsers, testFilePathBooks);
+            string expectedOutput =
+                "Welcome To Personal Library System\n\n\r\n1. Login\r\n2. Register\r\n3. Guest Mode\r\n4. Exit Program\r\nPlease enter a number to select:\r\nEnter email: Enter password: User registered successfully.\r\nPress any key to continue...\r\nWelcome To Personal Library System\n\n\r\n1. Login\r\n2. Register\r\n3. Guest Mode\r\n4. Exit Program\r\nPlease enter a number to select:\r\nGuest Operations\n\n\r\n1. View Catalog\r\n2. Return to Main Menu\r\nPlease enter a number to select:\r\nWelcome To Personal Library System\n\n\r\n1. Login\r\n2. Register\r\n3. Guest Mode\r\n4. Exit Program\r\nPlease enter a number to select:\r\nEnter email: Enter password: Login successful.\r\nPress any key to continue...\r\nWelcome to User Operations\n\n\r\n1. Book Cataloging\r\n2. Loan Management\r\n3. WishList Management\r\n4. Reading Tracker\r\n5. Return to Main Menu\r\nPlease enter a number to select:\r\nWelcome To Personal Library System\n\n\r\n1. Login\r\n2. Register\r\n3. Guest Mode\r\n4. Exit Program\r\nPlease enter a number to select:\r\nExit Program\r\n";
+
+            Assert.Equal(expectedOutput, output.ToString());
+            Assert.Equal(0, result);
+
+            // Clean up
+            Console.SetOut(new StreamWriter(Console.OpenStandardOutput()));
+            Console.SetIn(new StreamReader(Console.OpenStandardInput()));
+        }
         private void CreateTestFile()
         {
             //Books
