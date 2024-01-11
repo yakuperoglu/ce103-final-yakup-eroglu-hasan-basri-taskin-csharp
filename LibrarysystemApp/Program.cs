777f6d16784bfbafc9d650322739e14493531060
yakup <yakup_eroglu22@erdogan.edu.tr>
2024-01-11 13:09:15 +0300

    app main file created

--- LibrarysystemApp/Program.cs
+++ LibrarysystemApp/Program.cs
@@ -1,10 +1,31 @@
-﻿internal class Program {
-  private static void Main(string[] args) {
-    Console.WriteLine("Librarysystem Application Running..");
-    var librarysystemLibrary = new LibrarysystemLibrary.Librarysystem();
-    librarysystemLibrary.Add(2, 2);
-    librarysystemLibrary.Multiply(2, 2);
-    librarysystemLibrary.Subtract(2, 2);
-    librarysystemLibrary.Divide(2, 2);
-  }
+﻿/*
+
+@file Program.cs
+@brief The main entry point for the Librarysystem application.
+*/
+
+/*
+ 
+@class Program
+@brief Contains the main method to run the Librarysystem application.
+@details This class initializes the Librarysystem library and launches the main menu.
+*/
+internal class Program
+{
+    /**
+     
+@brief The main entry point for the application.
+@param args Command-line arguments passed to the program.*/
+    private static void Main(string[] args)
+    {// Define file paths for storing user and book information.
+        string pathFileUsers = "users.bin";
+        string pathFileBooks = "books.bin";
+
+        // Display application startup message.
+        Console.WriteLine("Librarysystem Application Running..");
+        // Initialize the Librarysystem library.
+        var librarysystemLibrary = new LibrarysystemLibrary.Librarysystem();
+        // Launch the main menu of the Librarysystem.
+        librarysystemLibrary.Main_Menu(pathFileUsers, pathFileBooks);
+    }
 }
